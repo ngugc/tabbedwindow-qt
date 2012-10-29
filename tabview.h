591e7b94cf318b12d24833c6eb1c70492d3f4e99
Daniele Esposti <expo@expobrain.net>
2012-10-29 09:50:51 +0000

    Improved doc string

--- tabview.h
+++ tabview.h
@@ -42,6 +42,9 @@ public:
     explicit TabView(QWidget *parent = 0);
 
     // Implementation of the QTabBar::tabAt method
+    // This should be exposed here because the original one is a protected
+    // method and in the TabbedWindow class we need to retrieve the tab's index
+    // by the cursor position.
     int tabAt(const QPoint &pos);
 };
 
